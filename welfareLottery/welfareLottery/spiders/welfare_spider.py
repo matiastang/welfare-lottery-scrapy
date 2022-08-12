b1f701eb58e8e8e7f5c4e9730cc956c6d8f6329c
唐道勇 <matias@tangdaoongdeMBP.lan>
2022-08-12 10:56:39 +0800

    feat: - 循环拉取所有可查询信息

--- welfareLottery/welfareLottery/spiders/welfare_spider.py
+++ welfareLottery/welfareLottery/spiders/welfare_spider.py
@@ -2,25 +2,47 @@
 Author: matiastang
 Date: 2022-08-09 17:50:16
 LastEditors: matiastang
-LastEditTime: 2022-08-11 12:16:00
+LastEditTime: 2022-08-12 10:52:27
 FilePath: /welfare-lottery-scrapy/welfareLottery/welfareLottery/spiders/welfare_spider.py
 Description: 
 '''
 import scrapy
 import json
+import datetime
+from time import strftime
 from welfareLottery.items import WelfarelotteryItem
+
 
 class WelfareSpider(scrapy.Spider):
 
     name = "welfare"
+    # 不用cookie也能访问
+    HMF_CI_Cookie = 'a60ab06db4b776d3110d0b4e993449f018834c2094daa81e2df06925d2c07539ead7b1c3420b3495a6a635940aeddf9d5b8855cc2f172714e5d48cb58c104db10e; 21_vq=4'
+    # 地址相关
+    url = 'http://www.cwl.gov.cn/cwl_admin/front/cwlkj/search/kjxx/findDrawNotice?name=ssq'
+    issueCount = ''
+    issueStart = ''
+    issueEnd = ''
+    dayStart = ''
+    dayEnd = ''
+    # 当年第一次请求（影响issueStart及issueEnd的期数）
+    isYearFirst = True
 
     def start_requests(self):
+        # 当前时间
+        now = datetime.datetime.now()
+        # 当前年
+        year = now.strftime('%Y')
+        # 当前年
+        self.nowYear = year
+        # 当前获取数据的年
+        self.requestYear = year
         urls = [
-            'http://www.cwl.gov.cn/cwl_admin/front/cwlkj/search/kjxx/findDrawNotice?name=ssq&issueCount=300&issueStart=&issueEnd=&dayStart=&dayEnd=',
+            self.url + '&issueStart=' + year + ('001' if self.isYearFirst else '100') + '&issueEnd=' + year + ('100' if self.isYearFirst else '200')
         ]
+
         for url in urls:
-            HMF_CI_Cookie = 'a60ab06db4b776d3110d0b4e993449f018834c2094daa81e2df06925d2c07539ead7b1c3420b3495a6a635940aeddf9d5b8855cc2f172714e5d48cb58c104db10e; 21_vq=4'
-            yield scrapy.Request(url=url, callback=self.parse, cookies={'HMF_CI': HMF_CI_Cookie})
+            yield scrapy.Request(url=url, callback=self.parse, cookies={'HMF_CI': self.HMF_CI_Cookie})
 
     def parse(self, response):
         bodyStr = str(response.body, encoding='utf-8')
@@ -50,3 +72,22 @@ class WelfareSpider(scrapy.Spider):
             '''
             for item in result:
                 yield WelfarelotteryItem(code=item['code'], date=item['date'], red=item['red'], blue=item['blue'])
+            countNum = body['countNum']
+            if self.requestYear == self.nowYear:
+                if countNum <= 0:
+                    self.isYearFirst = False
+            else:
+                if countNum <= 0:
+                    print('查询结束:' + self.requestYear + '年-' + self.nowYear + '年的数据')
+                    return
+
+        if self.isYearFirst is False:
+            # 查询上一年的数据
+            yearNum = int(self.requestYear)
+            self.requestYear = str(yearNum - 1)
+            print('查询上' + self.requestYear + '年的数据')
+
+        self.isYearFirst = bool(1-self.isYearFirst)
+
+        requestUrl = self.url + '&issueStart=' + self.requestYear + ('001' if self.isYearFirst else '100') + '&issueEnd=' + self.requestYear + ('100' if self.isYearFirst else '200')
+        yield scrapy.Request(url=requestUrl, callback=self.parse, cookies={'HMF_CI': self.HMF_CI_Cookie})
