b3bdccc0047c9c51130f05315fda18419c44e2da
唐道勇 <matias@tangdaoongdeMBP.lan>
2022-08-18 11:18:33 +0800

    feat: - 添加参数校验

--- welfareLottery/start.py
+++ welfareLottery/start.py
@@ -2,12 +2,13 @@
 Author: matiastang
 Date: 2022-08-11 11:13:01
 LastEditors: matiastang
-LastEditTime: 2022-08-18 11:01:18
+LastEditTime: 2022-08-18 11:17:30
 FilePath: /welfare-lottery-scrapy/welfareLottery/start.py
 Description: 启动
 '''
 #!/usr/bin/python3
 #coding=utf-8
+from ast import Try
 import sys
 from scrapy import cmdline
 
@@ -21,5 +22,10 @@ if __name__ == '__main__':
     if argvLen < 2:
         print('请添加welfare_last获取条数')
     else:
-        # 运行welfare_last
-        cmdline.execute(f'scrapy crawl welfare_last -a count={argv[argvLen - 1]}'.split())
+        try:
+            count = int(argv[argvLen - 1])
+            # 运行welfare_last
+            print('welfare_last获取条数需要在1~100') if count <= 0 or count > 100 else cmdline.execute(f'scrapy crawl welfare_last -a count={count}'.split())
+        except ValueError as e:
+            print('参数转换int错误', argv)
+        
