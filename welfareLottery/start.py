b3003f0a2206c4738bce8ea005d6e2b5e5df3fc6
唐道勇 <matias@tangdaoongdeMBP.lan>
2022-08-18 11:07:51 +0800

    feat: - welfare_last使用参数控制获取的条数

--- welfareLottery/start.py
+++ welfareLottery/start.py
@@ -2,9 +2,24 @@
 Author: matiastang
 Date: 2022-08-11 11:13:01
 LastEditors: matiastang
-LastEditTime: 2022-08-11 11:14:18
+LastEditTime: 2022-08-18 11:01:18
 FilePath: /welfare-lottery-scrapy/welfareLottery/start.py
 Description: 启动
 '''
+#!/usr/bin/python3
+#coding=utf-8
+import sys
 from scrapy import cmdline
-cmdline.execute('scrapy crawl welfare'.split())
+
+# process = CrawlerProcess()
+# process.crawl(MySpider, category="electronics")
+
+if __name__ == '__main__':
+
+    argv = sys.argv
+    argvLen = len(argv)
+    if argvLen < 2:
+        print('请添加welfare_last获取条数')
+    else:
+        # 运行welfare_last
+        cmdline.execute(f'scrapy crawl welfare_last -a count={argv[argvLen - 1]}'.split())
--- welfareLottery/welfareLottery/spiders/welfare_last_spider.py
+++ welfareLottery/welfareLottery/spiders/welfare_last_spider.py
@@ -2,7 +2,7 @@
 Author: matiastang
 Date: 2022-08-09 17:50:16
 LastEditors: matiastang
-LastEditTime: 2022-08-17 18:05:39
+LastEditTime: 2022-08-18 11:05:04
 FilePath: /welfare-lottery-scrapy/welfareLottery/welfareLottery/spiders/welfare_last_spider.py
 Description: 爬取到目前为止最新的数据
 '''
@@ -13,24 +13,22 @@ from welfareLottery.items import WelfarelotteryItem
 class WelfareLastSpider(scrapy.Spider):
 
     name = "welfare_last"
-    # allowed_domains = ['cwl.gov.cn']
+    allowed_domains = ['www.cwl.gov.cn']
 
-    # 地址相关
-    urls = ['http://www.cwl.gov.cn/cwl_admin/front/cwlkj/search/kjxx/findDrawNotice?name=ssq&issueCount=10']
+    def __init__(self, count=None, *args, **kwargs):
+        super(WelfareLastSpider, self).__init__(*args, **kwargs)
+        self.start_urls = f'http://www.cwl.gov.cn/cwl_admin/front/cwlkj/search/kjxx/findDrawNotice?name=ssq&issueCount={count}'
 
     def start_requests(self):
-        for url in self.urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+        yield scrapy.Request(url=self.start_urls, callback=self.parse)   
 
     def parse(self, response):
-        print(self.name + 'parse')
         bodyStr = str(response.body, encoding='utf-8')
-        # json字符串转换为dict
         body = json.loads(bodyStr)
         if body:
             result = body['result']
             for item in result:
                 yield WelfarelotteryItem(code=item['code'], date=item['date'], red=item['red'], blue=item['blue'])
     
-    # def closed(reason):
-    #     print('welfare_last_spider closed')
+    def closed(self, reason):
+        print('welfare_last_spider closed')
