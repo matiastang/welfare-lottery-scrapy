80c8ba3e22a7f100f6fbb8590d59bf7294c46210
唐道勇 <matias@tangdaoongdeMBP.lan>
2023-03-17 12:35:22 +0800

    feat: - 更新脚本文件

--- welfare-lottery-scrapy/stop.sh
+++ welfare-lottery-scrapy/stop.sh
@@ -7,4 +7,6 @@
  # @FilePath: /welfare-lottery-scrapy/welfare-lottery-scrapy/stop.sh
  # @Description: 更具文件名称结束任务
 ### 
+source /etc/profile
+nohup echo "hello world"
 ps -ef|grep "文件名称" | grep -v grep| awk '{print "kill -9 " $2}' 
