417eda9a73559908aded68199a328cc32fc47266
唐道勇 <matias@tangdaoongdeMBP.lan>
2023-11-10 10:58:38 +0800

    feat: - 更新文件路径

--- welfare-lottery-scrapy/reset.sh
+++ welfare-lottery-scrapy/reset.sh
@@ -3,7 +3,7 @@
  # @Author: matiastang
  # @Date: 2023-03-09 19:48:07
  # @LastEditors: matiastang
- # @LastEditTime: 2023-03-17 16:15:36
+ # @LastEditTime: 2023-11-10 10:58:12
  # @FilePath: /welfare-lottery-scrapy/welfare-lottery-scrapy/reset.sh
  # @Description: 重置定时任务
 ### 
@@ -12,16 +12,16 @@ source /etc/profile
 PID=`ps -ef | grep wl_apscheduler.py | grep -v grep | awk '{print $2}'`
 if [ "$PID" = "" ] 
 then 
-    echo "切换路径：/var/mt-project/mt-scrapy/welfare-lottery-scrapy/"
-    cd /var/mt-project/mt-scrapy/welfare-lottery-scrapy/
+    echo "切换路径：/var/mt-scrapy/welfare-lottery-scrapy/"
+    cd /var/mt-scrapy/welfare-lottery-scrapy/
     echo "启动：wl_apscheduler.py"
     NEWPID=`nohup python3 ./wl_apscheduler.py >>output.out 2>&1 &`
     echo "启动完成：$NEWPID"
 else
     echo "关闭任务：$PID"
     kill -9 $PID
-    echo "切换路径：/var/mt-project/mt-scrapy/welfare-lottery-scrapy/"
-    cd /var/mt-project/mt-scrapy/welfare-lottery-scrapy/
+    echo "切换路径：/var/mt-scrapy/welfare-lottery-scrapy/"
+    cd /var/mt-scrapy/welfare-lottery-scrapy/
     echo "重启：wl_apscheduler.py"
     NEWPID=`nohup python3 ./wl_apscheduler.py >>output.out 2>&1 &`
     echo "重启完成：$NEWPID"
